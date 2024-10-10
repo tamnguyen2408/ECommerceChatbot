6ffb09cc958ed7ce6b6089047d13149ff69847d9
tamnguyen2408 <nguyenngoctam082004@gmail.com>
2024-10-10 10:40:27 +0700

    editAdmin

---
+++ ECommerceChatbot/Areas/Admin/Controllers/ProductManagementController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceChatbot.Areas.Admin.Controllers
+{
+    [Area("Admin")] // "Admin" should be case-sensitive as it matches the folder name
+    [Route("Admin/ProductManagement")] // Use placeholder for better structure
+    public class ProductManagementController : Controller
+    {
+        [Route("Index")] // Maps to the Index action or leave it if default action
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ECommerceChatbot/obj/Debug/net8.0/ECommerceChatbot.AssemblyInfo.cs
+++ ECommerceChatbot/obj/Debug/net8.0/ECommerceChatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28fca82ae75504a28f951a6a52a058c2f4891ae9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a0aa6c3696ffb8e0807b87e159f4cfae85ab1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
