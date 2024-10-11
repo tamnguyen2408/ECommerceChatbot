e3f59cec8eeca03abe197638fd1a717895d38e87
Duong123456798 <leduonggsf171004@gmail.com>
2024-10-11 22:32:01 +0700

    update function admin

--- ECommerceChatbot/obj/Debug/net8.0/ECommerceChatbot.AssemblyInfo.cs
+++ ECommerceChatbot/obj/Debug/net8.0/ECommerceChatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a0aa6c3696ffb8e0807b87e159f4cfae85ab1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ffb09cc958ed7ce6b6089047d13149ff69847d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
